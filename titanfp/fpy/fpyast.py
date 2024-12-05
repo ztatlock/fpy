c9f59e275b15254e457a0fff75560c455cf977ae
Brett Saiki <bksaiki@gmail.com>
2024-12-05 10:30:40 -0800

    all operators

--- titanfp/fpy/fpyast.py
+++ titanfp/fpy/fpyast.py
@@ -87,94 +87,267 @@ class NaryExpr(Expr):
     name: str
     children: list[Expr]
 
-    def __init__(self, name: str, *children: Expr) -> None:
-        self.name = name
+    def __init__(self, *children: Expr):
         self.children = list(children)
-
-class UnknownCall(NaryExpr):
-    """FPy node: abstract application"""
 
 class UnaryExpr(NaryExpr):
     """FPy node: abstract unary application"""
 
-class Neg(UnaryExpr):
-    """FPy node: subtraction expression"""
-    
     def __init__(self, e: Expr):
-        super().__init__('-', e)
-
-class Fabs(UnaryExpr):
-    """FPy node: absolute value expression"""
-    
-    def __init__(self, e: Expr):
-        super().__init__('fabs', e)
-
-class Sqrt(UnaryExpr):
-    """FPy node: square-root expression"""
-    
-    def __init__(self, e: Expr):
-        super().__init__('sqrt', e)
+        super().__init__(e)
 
 class BinaryExpr(NaryExpr):
+    """FPy node: abstract binary application"""
+
+    def __init__(self, e1: Expr, e2: Expr):
+        super().__init__(e1, e2)
+
+class TernaryExpr(NaryExpr):
     """FPy node: abstract ternary application"""
 
+    def __init__(self, e1: Expr, e2: Expr, e3: Expr):
+        super().__init__(e1, e2, e3)
+
+class UnknownCall(NaryExpr):
+    """FPy node: abstract application"""
+
+# IEEE 754 required arithmetic
+
 class Add(BinaryExpr):
-    """FPy node: subtraction expression"""
-    
-    def __init__(self, e1: Expr, e2: Expr):
-        super().__init__('+', e1, e2)
+    """FPy node: addition"""
+    name: str = '+'
 
 class Sub(BinaryExpr):
-    """FPy node: subtraction expression"""
-    
-    def __init__(self, e1: Expr, e2: Expr):
-        super().__init__('-', e1, e2)
+    """FPy node: subtraction"""
+    name: str = '-'
 
 class Mul(BinaryExpr):
-    """FPy node: subtraction expression"""
-
-    def __init__(self, e1: Expr, e2: Expr):
-        super().__init__('*', e1, e2)
+    """FPy node: subtraction"""
+    name: str = '*'
     
 class Div(BinaryExpr):
-    """FPy node: subtraction expression"""
+    """FPy node: subtraction"""
+    name: str = '/'
 
-    def __init__(self, e1: Expr, e2: Expr):
-        super().__init__('/', e1, e2)
+class Fabs(UnaryExpr):
+    """FPy node: absolute value"""
+    name: str = 'fabs'
 
-class Sin(UnaryExpr):
-    """FPy node: sine expression"""
+class Sqrt(UnaryExpr):
+    """FPy node: square-root"""
+    name: str = 'sqrt'
 
-    def __init__(self, e: Expr):
-        super().__init__('sin', e)
+class Fma(TernaryExpr):
+    """FPy node: square-root"""
+    name: str = 'fma'
 
-class Cos(UnaryExpr):
-    """FPy node: cosine expression"""
+# Sign operations
 
-    def __init__(self, e: Expr):
-        super().__init__('cos', e)
+class Neg(UnaryExpr):
+    """FPy node: negation"""
+    name: str = '-'
 
-class Tan(UnaryExpr):
-    """FPy node: tangent expression"""
+class Copysign(BinaryExpr):
+    """FPy node: copysign"""
+    name: str = 'copysign'
 
-    def __init__(self, e: Expr):
-        super().__init__('tan', e)
+# Composite arithmetic
+
+class Fdim(BinaryExpr):
+    """FPy node: `max(x - y, 0)`"""
+    name: str = 'fdim'
+
+class Fmax(BinaryExpr):
+    """FPy node: `max(x, y)`"""
+    name: str = 'fmax'
+
+class Fmin(BinaryExpr):
+    """FPy node: `min(x, y)`"""
+    name: str = 'fmin'
+
+class Fmod(BinaryExpr):
+    name: str = 'fmod'
+
+class Remainder(BinaryExpr):
+    name: str = 'remainder'
+
+class Hypot(BinaryExpr):
+    """FPy node: `sqrt(x ** 2 + y ** 2)`"""
+    name: str = 'hypot'
+
+# Other arithmetic
+
+class Cbrt(UnaryExpr):
+    """FPy node: cube-root"""
+    name: str = 'cbrt'
+
+# Rounding and truncation
+
+class Ceil(UnaryExpr):
+    """FPy node: ceiling"""
+    name: str = 'ceil'
+
+class Floor(UnaryExpr):
+    """FPy node: floor"""
+    name: str = 'floor'
+
+class Nearbyint(UnaryExpr):
+    """FPy node: nearby-integer"""
+    name: str = 'nearbyint'
+
+class Round(UnaryExpr):
+    """FPy node: round"""
+    name: str = 'round'
+
+class Trunc(UnaryExpr):
+    """FPy node: truncation"""
+    name: str = 'trunc'
+
+# Trigonometric functions
+
+class Acos(UnaryExpr):
+    """FPy node: inverse cosine"""
+    name: str = 'acos'
+
+class Asin(UnaryExpr):
+    """FPy node: inverse sine"""
+    name: str = 'asin'
 
 class Atan(UnaryExpr):
-    """FPy node: inverse tangent expression"""
+    """FPy node: inverse tangent"""
+    name: str = 'atan'
 
-    def __init__(self, e: Expr):
-        super().__init__('atan', e)
+class Atan2(BinaryExpr):
+    """FPy node: `atan(y / x)` with correct quadrant"""
+    name: str = 'atan2'
+
+class Cos(UnaryExpr):
+    """FPy node: cosine"""
+    name: str = 'cos'
+
+class Sin(UnaryExpr):
+    """FPy node: sine"""
+    name: str = 'sin'
+
+class Tan(UnaryExpr):
+    """FPy node: tangent"""
+    name: str = 'tan'
+
+# Hyperbolic functions
+
+class Acosh(UnaryExpr):
+    """FPy node: inverse hyperbolic cosine"""
+    name: str = 'acosh'
+
+class Asinh(UnaryExpr):
+    """FPy node: inverse hyperbolic sine"""
+    name: str = 'asinh'
+
+class Atanh(UnaryExpr):
+    """FPy node: inverse hyperbolic tangent"""
+    name: str = 'atanh'
+
+class Cosh(UnaryExpr):
+    """FPy node: hyperbolic cosine"""
+    name: str = 'cosh'
+
+class Sinh(UnaryExpr):
+    """FPy node: hyperbolic sine"""
+    name: str = 'sinh'
+
+class Tanh(UnaryExpr):
+    """FPy node: hyperbolic tangent"""
+    name: str = 'tanh'
+
+# Exponential / logarithmic functions
+
+class Exp(UnaryExpr):
+    """FPy node: exponential (base e)"""
+    name: str = 'exp'
+
+class Exp2(UnaryExpr):
+    """FPy node: exponential (base 2)"""
+    name: str = 'exp2'
+
+class Expm1(UnaryExpr):
+    """FPy node: `exp(x) - 1`"""
+    name: str = 'expm1'
+
+class Log(UnaryExpr):
+    """FPy node: logarithm (base e)"""
+    name: str = 'log'
+
+class Log10(UnaryExpr):
+    """FPy node: logarithm (base 10)"""
+    name: str = 'log10'
+
+class Log1p(UnaryExpr):
+    """FPy node: `log(x + 1)`"""
+    name: str = 'log1p'
+
+class Log2(UnaryExpr):
+    """FPy node: logarithm (base 2)"""
+    name: str = 'log2'
+
+class Pow(BinaryExpr):
+    """FPy node: `x ** y`"""
+    name: str = 'pow'
+
+# Integral functions
+
+class Erf(UnaryExpr):
+    """FPy node: error function"""
+    name: str = 'erf'
+
+class Erfc(UnaryExpr):
+    """FPy node: complementary error function"""
+    name: str = 'erfc'
+
+class Lgamma(UnaryExpr):
+    """FPy node: logarithm of the absolute value of the gamma function"""
+    name: str = 'lgamma'
+
+class Tgamma(UnaryExpr):
+    """FPy node: gamma function"""
+    name: str = 'tgamma'
+
+# Classification
+
+class IsFinite(UnaryExpr):
+    """FPy node: is the value finite?"""
+    name: str = 'isfinite'
+
+class IsInf(UnaryExpr):
+    """FPy node: is the value infinite?"""
+    name: str = 'isinf'
+
+class IsNan(UnaryExpr):
+    """FPy node: is the value NaN?"""
+    name: str = 'isnan'
+
+class IsNormal(UnaryExpr):
+    """FPy node: is the value normal?"""
+    name: str = 'isnormal'
+
+class Signbit(UnaryExpr):
+    """FPy node: is the signbit 1?"""
+    name: str = 'signbit'
+
+# Logical operators
+
+class Not(UnaryExpr):
+    """FPy node: logical negation"""
+    name: str = 'not'
 
 class Or(NaryExpr):
-    """FPy node: || expression"""
-    def __init__(self, *children: Expr):
-        super().__init__('or', *children)
+    """FPy node: logical disjunction"""
+    name: str = 'or'
 
 class And(NaryExpr):
-    """FPy node: && expression"""
-    def __init__(self, *children: Expr):
-        super().__init__('and', *children)
+    """FPy node: logical conjunction"""
+    name: str = 'and'
+
+# Comparisons
 
 class CompareOp(Enum):
     LT = 0
@@ -196,6 +369,8 @@ class Compare(Expr):
             raise TypeError(f'expected list of length >= {len(children)}', children)
         self.ops = ops
         self.children = children
+
+# Statements
 
 class Stmt(Ast):
     """FPy node: abstract statement"""
